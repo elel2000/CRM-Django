7c934ad5681fde6a619c63562cffc9a7faf82289
elel2000 <hamza.elalamy1@uit.ac.ma>
2023-07-28 23:36:04 +0100

    Initial commit

---
+++ commande/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from client.models import Client
+from produit.models import Produit
+# Create your models here.
+
+class Commande(models.Model):
+    STATUS = (('en instance','en instance'),('Non livré','Non livré'),('livré','livré'))
+    client = models.ForeignKey(Client,null=True,on_delete=models.SET_NULL)
+    produits = models.ManyToManyField(Produit)
+    date_creation = models.DateTimeField(auto_now_add=True,null=True)
+    status = models.CharField(max_length=200,null=True,choices=STATUS)
